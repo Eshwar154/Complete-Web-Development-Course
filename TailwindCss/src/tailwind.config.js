62e7888e0e34f8b30f67bc21e488fc2c3f3f8505
Eshwar154 <eshwarpaygude@gmail.com>
2026-01-26 14:30:56 +0530

    Tailwind Css

---
+++ TailwindCss/src/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"], // This tells Tailwind to watch all HTML files in this folder
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
